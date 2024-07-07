3560f599b01379d845d8d9632d907eba03d90c84
pcaldi <pcaldi@gmail.com>
2024-07-07 13:19:12 -0300

    Estilo global

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import { ThemeProvider } from 'styled-components'
+import theme from './styles/theme'
+import GlobalStyles from './styles/global'
 
 import { Details } from './pages/Details'
-import theme from './styles/theme'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <GlobalStyles />
       <Details />
     </ThemeProvider>
   </React.StrictMode>
--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -4,8 +4,7 @@ export const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100vh;
-  background: red;
+
 `
 
 
