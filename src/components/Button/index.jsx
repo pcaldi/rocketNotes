ea0c62236660bb74eb294795d78b538705e52098
pcaldi <pcaldi@gmail.com>
2024-07-07 13:59:38 -0300

    Component Button

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,10 @@
+import { Container } from "./styles";
+
+
+export function Button() {
+  return (
+    <Container>
+      Entrar
+    </Container>
+  )
+}
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,10 +1,12 @@
-import {Container} from "./styles"
+import { Button } from "../../components/Button"
+import { Container } from "./styles"
 
 export function Details() {
   return (
     <Container>
       <h1>Hello World!</h1>
       <span>Paulo Ricardo Caldi</span>
+      <Button />
     </Container>
   )
 }
