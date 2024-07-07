7e4dce7aaa0532229b213cb20e6c641cdacd83e1
pcaldi <pcaldi@gmail.com>
2024-07-07 12:38:10 -0300

    Definindo o Tema da aplicação

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,9 +1,10 @@
+import {Container} from "./styles"
 
 export function Details() {
   return (
-    <>
+    <Container>
       <h1>Hello World!</h1>
       <span>Paulo Ricardo Caldi</span>
-    </>
+    </Container>
   )
 }
