254c9a6dab5b3721fff8bf6d0f01852ea18955a5
pcaldi <pcaldi@gmail.com>
2024-07-11 12:33:44 -0300

    Adicionando o component Section

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -2,11 +2,21 @@ import { Container } from "./styles"
 
 import { Header } from "../../components/Header"
 import { Button } from "../../components/Button"
+import { Section } from "../../components/Section"
 
 export function Details() {
   return (
     <Container>
       <Header />
+
+      <Section title="Links úteis">
+        <ul>
+          <li>item 1</li>
+          <li>item 2</li>
+          <li>item 3</li>
+        </ul>
+      </Section>
+
       <Button title="Voltar"/>
     </Container>
   )
