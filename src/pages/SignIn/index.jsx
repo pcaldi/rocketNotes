5e541f72421345cc3448d560f603f3ace74c4fc3
pcaldi <pcaldi@gmail.com>
2024-07-12 14:00:49 -0300

    Criando a page de SingIn

---
+++ src/pages/SignIn/index.jsx
@@ -0,0 +1,38 @@
+import { Container, Form } from "./styles";
+
+import { LuMail, LuLock } from "react-icons/lu";
+
+import { Input } from "../../components/Input"
+import { Button } from "../../components/Button"
+import { ButtonText } from "../../components/ButtonText"
+
+export function SignIn() {
+  return (
+    <Container>
+      <Form>
+        <h1>Rocket Notes</h1>
+        <p>Aplicação para salvar e gerenciar seus links úteis.</p>
+
+        <h2>Faça seu login</h2>
+
+        <Input
+          placeholder="E-mail"
+          icon={LuMail}
+          type="text"
+        />
+
+        <Input
+          placeholder="Senha"
+          type="password"
+          icon={LuLock}
+        />
+
+        <Button title="Entrar" />
+
+        <ButtonText title="Criar conta" isactive />
+      </Form>
+
+
+    </Container>
+  )
+}
