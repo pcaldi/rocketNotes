fdc6fd8b00354a051acc21e7b8eb8c507d9aad50
pcaldi <pcaldi@gmail.com>
2024-07-12 16:54:16 -0300

    Estilizando o NoteItem

--- src/components/NoteItem/index.jsx
+++ src/components/NoteItem/index.jsx
@@ -17,6 +17,7 @@ export function NoteItem({ isNew, value, onClick, ...rest }) {
       <button
         type="button"
         onClick={onClick}
+        className={isNew ? 'button-add' : 'button-delete'}
       >
         {isNew ? <LuPlus /> : <LuX />}
       </button>
--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -3,6 +3,8 @@ import { Container, Form } from "./styles";
 import { Header } from "../../components/Header"
 import { Input } from "../../components/Input"
 import { Textarea } from "../../components/Textarea";
+import { Section } from "../../components/Section";
+import { NoteItem } from "../../components/NoteItem";
 
 export function New() {
   return (
@@ -22,6 +24,12 @@ export function New() {
           />
 
           <Textarea placeholder="Observações" />
+
+          <Section title="Links úteis">
+            <NoteItem value="https://github.com/pcaldi" />
+            <NoteItem isNew />
+
+          </Section>
         </Form>
       </main>
 
