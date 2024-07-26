ae2ec1b703cac946fccfd6a837d4193f372c1cbb
pcaldi <pcaldi@gmail.com>
2024-07-26 16:18:05 -0300

    carregando as tags

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,10 @@
+import { useState, useEffect } from 'react';
+
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles'
 
 import { FiPlus } from "react-icons/fi";
 
+import { api } from '../../services/api'
 
 import { Note } from '../../components/Note'
 import { Input } from '../../components/Input'
@@ -10,6 +13,19 @@ import { Section } from '../../components/Section'
 import { ButtonText } from '../../components/ButtonText'
 
 export function Home() {
+  const [tags, setTags] = useState([]);
+
+  useEffect(() => {
+    async function fetchTags() {
+      // Pego a resposta da api pegando as tags
+      const response = await api.get('/tags');
+      // Guardo os dados em um estado
+      setTags(response.data);
+    }
+
+    fetchTags();
+  }, []);
+
   return (
     <Container>
       <Brand>
@@ -23,17 +39,14 @@ export function Home() {
           <ButtonText title="Todos" isactive />
         </li>
 
-        <li>
-          <ButtonText title="Frontend" />
-        </li>
+        { // Se existir tags eu pego a tags e percorro utilizando o map.
+          tags && tags.map(tag => (
+            <li key={tag.id}>
+              <ButtonText title={tag.name} />
+            </li>
+          ))
+        }
 
-        <li>
-          <ButtonText title="Node" />
-        </li>
-
-        <li>
-          <ButtonText title="React" />
-        </li>
       </Menu>
 
       <Search>
